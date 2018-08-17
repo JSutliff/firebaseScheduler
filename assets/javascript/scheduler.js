84c6816c7d07cec92d5231f5e181c088b00cd92f
Jason Sutliff <jbopsut@gmail.com>
2018-08-16 23:21:26 -0700

    attempted implementation of momentJS still needs calculations

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -14,24 +14,25 @@ function addNewTrain() {
   var trainName = $("#trainName").val().trim();
   var destination = $("#destination").val().trim();
   var firstTrain = $("#firstTrain").val().trim();
-  var frequency = $("#frequency").val().trim();
-
+  var frequency = parseInt($("#frequency").val().trim());
+  
   // append values train information to html table
-  var newTableRow = $('<tr>');
-  newTableRow.append('<td>' + trainName + '</td>' );
-  newTableRow.append('<td>' + destination + '</td>' );
-  newTableRow.append('<td>' + firstTrain + '</td>' );
-  newTableRow.append('<td>' + frequency + '</td>' );
-  newTableRow.append('<td>CALCULATE WITH MOMENTJS</td>' );
+  // var newTableRow = $('<tr>');
+  // newTableRow.append('<td>' + trainName + '</td>' );
+  // newTableRow.append('<td>' + destination + '</td>' );
+  // newTableRow.append('<td>' + firstTrain + '</td>' );
+  // newTableRow.append('<td>' + frequency + '</td>' );
+  
 
-  $('#appendTrainInfoHere').append(newTableRow);
+  // $('#appendTrainInfoHere').append(newTableRow);
 
   // create temp object to hold train data
   var newTrain = {
     name: trainName,
     destination: destination,
     firstTrain: firstTrain,
-    frequency: frequency
+    frequency: frequency,
+    currentDate: firebase.database.ServerValue.TIMESTAMP
   };
 
   // sends train info to firebase
@@ -42,6 +43,7 @@ function addNewTrain() {
   console.log(newTrain.destination);
   console.log(newTrain.firstTrain);
   console.log(newTrain.frequency);
+  console.log(newTrain.date);
 
   // clear text
   $("#trainName").val("");
@@ -53,5 +55,22 @@ function addNewTrain() {
 $("#addTrainButton").on("click", function(event) {
   event.preventDefault();
   addNewTrain();
+});
+
+db.ref().orderByChild("currentDate").on("child_added", function(snapshot){
+  var ss = snapshot.val();
+  console.log(ss);
+  var frequency = ss.frequency;
+  var currentTime = moment().format("HH:mm");
+  currentTime.split(':')
   
+  
+  var newTableRow = $('<tr>');
+  newTableRow.append('<td>' + ss.name + '</td>' );
+  newTableRow.append('<td>' + ss.destination + '</td>' );
+  newTableRow.append('<td>' + frequency + '</td>' );
+  newTableRow.append('<td>' + ss.firstTrain + '</td>' );
+  // newTableRow.append('<td>' + minutesAway + '</td>' );
+  
+  $('#appendTrainInfoHere').append(newTableRow);
 });
