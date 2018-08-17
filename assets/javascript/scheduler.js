992d127d11ba6ef637134e4b8cf255e6095ef33e
Jason Sutliff <jbopsut@gmail.com>
2018-08-16 21:46:24 -0700

    addNewTrain function added, appends train info to table and updates database with info

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -10,3 +10,48 @@ firebase.initializeApp(config);
 
 var db = firebase.database();
 
+function addNewTrain() {
+  var trainName = $("#trainName").val().trim();
+  var destination = $("#destination").val().trim();
+  var firstTrain = $("#firstTrain").val().trim();
+  var frequency = $("#frequency").val().trim();
+
+  // append values train information to html table
+  var newTableRow = $('<tr>');
+  newTableRow.append('<td>' + trainName + '</td>' );
+  newTableRow.append('<td>' + destination + '</td>' );
+  newTableRow.append('<td>' + firstTrain + '</td>' );
+  newTableRow.append('<td>' + frequency + '</td>' );
+  newTableRow.append('<td>CALCULATE WITH MOMENTJS</td>' );
+
+  $('#appendTrainInfoHere').append(newTableRow);
+
+  // create temp object to hold train data
+  var newTrain = {
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  };
+
+  // sends train info to firebase
+  db.ref().push(newTrain);
+
+  // logs assumptions to the console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  // clear text
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#firstTrain").val("");
+  $("#frequency").val("");
+}
+
+$("#addTrainButton").on("click", function(event) {
+  event.preventDefault();
+  addNewTrain();
+  
+});
