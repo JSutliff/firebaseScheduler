31c23ebbdc8dd10d24bb4dd3b1ea0465c68c05b5
Jason Sutliff <jbopsut@gmail.com>
2018-08-16 21:10:28 -0700

    firebase and momentJS linked

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,12 @@
+var config = {
+  apiKey: "AIzaSyBo48A4WbkDSZ3ZXoLwq0wIUi78N_S2P3o",
+  authDomain: "testspace-ucb1.firebaseapp.com",
+  databaseURL: "https://testspace-ucb1.firebaseio.com",
+  projectId: "testspace-ucb1",
+  storageBucket: "testspace-ucb1.appspot.com",
+  messagingSenderId: "750641850621"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
